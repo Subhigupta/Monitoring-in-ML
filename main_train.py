be61c04968fc8b7632b726beeb83a2426998a4bd
Subhigupta <subhiguptasfc@gmail.com>
2025-12-23 13:29:19 +0530

    Renamed the file

--- main_train.py
+++ main_train.py
@@ -1,14 +1,18 @@
 from src.data.make_dataset import load_data
 from src.features.build_features import create_features
 from src.models.train_model import rf_clf, xgb_clf
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 # Load training data
+logger.info("Loading Training data...")
 train_df = load_data()
 print(train_df.shape)
 
 # Conduct feature engineering
+logger.info("Conducting Feature Engineering...")
 processed_train_df = create_features(train_df)
-print(processed_train_df.shape, processed_train_df.columns)
 
 # Train Random Forest and Xgboost classifiers
 rf_clf(processed_train_df)
