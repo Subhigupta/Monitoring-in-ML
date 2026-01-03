2cb884235ef00020b9b7dc52ab367fbebcc03842
Subhigupta <subhiguptasfc@gmail.com>
2026-01-03 22:46:58 +0530

    Modified the code;Added splitting of data into train, valiadtion and A/B testing holdout dataset

--- main_train.py
+++ main_train.py
@@ -1,7 +1,11 @@
 from src.data.make_dataset import load_data
 from src.features.build_features import create_features
 from src.models.train_model import rf_clf, xgb_clf
+from src.ab_testing.data_splitter import ABTestDataSplitter
 from src.utils.logger import get_logger
+from src.utils.config import load_config
+
+config = load_config()
 
 logger = get_logger(__name__)
 
@@ -14,6 +18,11 @@ print(train_df.shape)
 logger.info("Conducting Feature Engineering...")
 processed_train_df = create_features(train_df)
 
+# Split the data
+splitter = ABTestDataSplitter(config["ab_testing"]["data_split"])
+train_df, val_df, ab_test_df = splitter.split(processed_train_df)
+
 # Train Random Forest and Xgboost classifiers
-rf_clf(processed_train_df)
-xgb_clf(processed_train_df)
+rf_clf(train_df, val_df)
+xgb_clf(train_df, val_df)
+
