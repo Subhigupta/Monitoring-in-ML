36b181b297b99b8ec220dcf8008b984ae7e42014
Subhigupta <subhiguptasfc@gmail.com>
2026-01-03 23:25:39 +0530

    Removed the sklearn train_test_split command

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,39 +1,47 @@
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 
 from src.models.classifier import SklearnClassifier
 from src.utils.config import load_config
 from src.utils.store import AssignmentStore
+from src.utils.logger import get_logger
 
+logger = get_logger(__name__)
 config = load_config()
 store = AssignmentStore()
 
-def rf_clf(processed_train_df):
+def rf_clf(train_df, val_df):
 
-    df_train, df_test = train_test_split(processed_train_df, test_size=config["test_size"], 
-                                         random_state=config["random_state"])
+    df_train, df_test = train_df, val_df
 
-    rf_estimator = RandomForestClassifier(**config["random_forest"])
+    rf_estimator = RandomForestRegressor(**config["random_forest"])
     model = SklearnClassifier(rf_estimator, config["features"], config["target"])
+    logger.info("Fitting Random Forest Regressor...")
     model.train(df_train)
 
+    logger.info("Evaluating Random Forest Regressor...")
     metrics = model.evaluate(df_test)
 
+    logger.info("Saving Random Forest Regressor...")
     store.put_rf_model("rf_model.onnx", model.clf)
+
+    logger.info("Saving Random Forest Regressor Evaluation metrics...")
     store.put_metrics("rf_metrics.json", metrics)
 
-def xgb_clf(processed_train_df):
+def xgb_clf(train_df, val_df,):
 
-    df_train, df_test = train_test_split(processed_train_df, test_size=config["test_size"], 
-                                         random_state=config["random_state"])
-
-    xgb_estimator = XGBClassifier(**config["xgboost"])
+    df_train, df_test = train_df, val_df
+    xgb_estimator = XGBRegressor(**config["xgboost"])
     model = SklearnClassifier(xgb_estimator, config["features"], config["target"])
+    logger.info("Fitting Xgboost Regressor...")
     model.train(df_train)
 
+    logger.info("Evaluating Xgboost Regressor...")
     metrics = model.evaluate(df_test)
 
-    store.put_xgb_model("xgb_model.pkl", model.clf)
+    logger.info("Saving Xgboost Regressor...")
+    store.put_xgb_model("xgb_model.onnx", model.clf)
+
+    logger.info("Saving Xgboost Regressor Evaluation metrics...")
     store.put_metrics("xgb_metrics.json", metrics)
 
